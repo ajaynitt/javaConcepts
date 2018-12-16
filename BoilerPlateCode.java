42121334bdd7ece9b83717af9aafc36e82ff2854
ajaynitt <ajay.nittrichy@gmail.com>
2018-12-16 18:34:17 +0530

    Update BoilerPlateCode.java

--- BoilerPlateCode.java
+++ BoilerPlateCode.java
@@ -10,3 +10,32 @@ public class Pet {
     private String name;
     private Person owner;
 }
+
+
+
+======================boilerPlate code for above code =================
+    public class Pet {
+    private String name;
+    private Person owner;
+
+    public Pet(String name, Person owner) {
+        this.name = name;
+        this.owner = owner;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Person getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Person owner) {
+        this.owner = owner;
+    }
+}
